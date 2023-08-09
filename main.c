0a163b793eddf2ac65a1ada8e7dab53e33f6d078
ArtemiyGranat <artemiigranat@gmail.com>
2023-08-09 19:09:23 +0400

    bruteforce of a 3-char alphabet

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+
+void
+bruteforce (char *alphabet)
+{
+  size_t alph_size = strlen (alphabet);
+  for (size_t i = 0; i < alph_size; ++i)
+    {
+      for (size_t j = 0; j < alph_size; ++j)
+        {
+          for (size_t k = 0; k < alph_size; ++k)
+            {
+              printf ("%c%c%c\n", alphabet[i], alphabet[j], alphabet[k]);
+            }
+        }
+    }
+}
 
 int
 main (void)
 {
-  printf ("Hello, brute!");
+  char *alphabet = "abc";
+  bruteforce (alphabet);
 }
