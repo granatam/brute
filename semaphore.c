20603ae871f9f6fb26c7a2bf4d6090e79642d1aa
ArtemiyGranat <artemiigranat@gmail.com>
2023-09-12 23:29:39 +0400

    added cleanup handler in semaphore.h

--- semaphore.c
+++ semaphore.c
@@ -9,7 +9,6 @@ sem_init (sem_t *sem, int pshared, unsigned int value)
     {
       return S_FAILURE;
     }
-
   if (pthread_mutex_init (&sem->mutex, NULL) != 0)
     {
       return S_FAILURE;
@@ -25,6 +24,7 @@ sem_post (sem_t *sem)
     {
       return S_FAILURE;
     }
+  pthread_cleanup_push (cleanup_mutex_unlock, &sem->mutex);
   if (sem->counter++ == 0)
     {
       if (pthread_cond_signal (&sem->cond_sem) != 0)
@@ -32,10 +32,7 @@ sem_post (sem_t *sem)
           return S_FAILURE;
         }
     }
-  if (pthread_mutex_unlock (&sem->mutex) != 0)
-    {
-      return S_FAILURE;
-    }
+  pthread_cleanup_pop (!0);
 
   return S_SUCCESS;
 }
@@ -47,6 +44,7 @@ sem_wait (sem_t *sem)
     {
       return S_FAILURE;
     }
+  pthread_cleanup_push (cleanup_mutex_unlock, &sem->mutex);
   while (sem->counter == 0)
     {
       if (pthread_cond_wait (&sem->cond_sem, &sem->mutex) != 0)
@@ -55,10 +53,7 @@ sem_wait (sem_t *sem)
         }
     }
   --sem->counter;
-  if (pthread_mutex_unlock (&sem->mutex) != 0)
-    {
-      return S_FAILURE;
-    }
+  pthread_cleanup_pop (!0);
 
   return S_SUCCESS;
 }
