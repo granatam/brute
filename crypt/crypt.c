33b2202653e16e56d153a0558b4c731b9537a457
ArtemiyGranat <artemiigranat@gmail.com>
2023-09-23 21:31:09 +0400

    added GNU license to crypt lib and some cleanup

--- crypt/crypt.c
+++ crypt/crypt.c
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 1991-2018 Free Software Foundation, Inc.
+ *
+ * The GNU C Library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The GNU C Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the GNU C Library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #include "crypt.h"
 
 #include <stdlib.h>
@@ -15,12 +33,6 @@ __crypt_r (const char *key, const char *salt,
   /* Try to find out whether we have to use MD5 encryption replacement.  */
   if (strncmp (md5_salt_prefix, salt, sizeof (md5_salt_prefix) - 1) == 0)
     {
-  //     /* FIPS rules out MD5 password encryption.  */
-  //     if (fips_enabled_p ())
-	// {
-	//   __set_errno (EPERM);
-	//   return NULL;
-	// }
       return __md5_crypt_r (key, salt, (char *) data,
 			    sizeof (struct crypt_data));
     }
@@ -44,13 +56,6 @@ __crypt_r (const char *key, const char *salt,
       __set_errno (EINVAL);
       return NULL;
     }
-
-  /* FIPS rules out DES password encryption.  */
-  // if (fips_enabled_p ())
-  //   {
-  //     __set_errno (EPERM);
-  //     return NULL;
-  //   }
 
   /*
    * Setup key schedule
