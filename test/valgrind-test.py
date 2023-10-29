c3ff3b6fcf09b3a9829221ae3e28204253577887
ArtemiyGranat <artemiigranat@gmail.com>
2023-10-29 07:37:11 +0400

    temp test change

--- test/valgrind-test.py
+++ test/valgrind-test.py
@@ -3,10 +3,8 @@ from hypothesis import given, strategies as st, settings
 from utils import gen_str, run_valgrind
 
 
-@given(st.text(min_size=1, max_size=1, alphabet='m'), st.text(min_size=1, max_size=1, alphabet='y'))
-@settings(deadline=timedelta(minutes=2))
-def test_valgrind(run_mode, brute_mode):
+def test_valgrind():
     alph = gen_str(5)
     passwd = gen_str(5, alph)
 
-    assert run_valgrind(passwd, alph, run_mode, brute_mode)
+    assert run_valgrind(passwd, alph, 'm', 'y')
