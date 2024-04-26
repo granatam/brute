57149122b5b4cc88454a832ff4705e366492644a
ArtemiyGranat <artemiigranat@gmail.com>
2024-04-26 16:26:17 +0400

    add debug messages and fix segfault

--- async_client.c
+++ async_client.c
@@ -22,15 +22,20 @@ client_worker (void *arg)
     .data = { .initialized = 0 },
   };
 
+  print_error("[sender] After st_ctx init\n");
   while (true)
     {
       task_t task;
       queue_pop (&ctx->task_queue, &task);
+      print_error("[sender] After task_queue pop\n");
       bool is_found
           = brute (&task, ctx->config, st_password_check, &st_context);
+      print_error("[sender] After brute\n");
       task.task.is_correct = is_found;
+      print_error("[sender] After is_correct set\n");
 
       queue_push (&ctx->result_queue, &task.task);
+      print_error("[sender] After result_queue push\n");
     }
   return (NULL);
 }
@@ -52,7 +57,7 @@ task_receiver (void *arg)
           print_error ("Could not receive command from server\n");
           goto end;
         }
-
+      print_error("[receiver] Received command\n");
       switch (cmd)
         {
         case CMD_ALPH:
@@ -61,6 +66,7 @@ task_receiver (void *arg)
               print_error ("Could not handle alphabet\n");
               goto end;
             }
+            print_error("[receiver] Received alph\n");
           break;
         case CMD_HASH:
           if (handle_hash (ctx->socket_fd, hash) == S_FAILURE)
@@ -68,9 +74,12 @@ task_receiver (void *arg)
               print_error ("Could not handle hash\n");
               goto end;
             }
+          print_error("[receiver] Received hash %s at %p, ctx->config->hash is %p\n", hash, hash, ctx->config->hash);
           ctx->config->hash = hash;
+          print_error("[receiver] After hash setting\n");
           break;
         case CMD_EXIT:
+          print_error("[receiver] Received exit\n");
           goto end;
         case CMD_TASK:
           if (recv_wrapper (ctx->socket_fd, &task, sizeof (task_t), 0)
@@ -79,7 +88,9 @@ task_receiver (void *arg)
               print_error ("Could not receive task from server\n");
               goto end;
             }
+          print_error("[receiver] Received task\n");
           queue_push (&ctx->task_queue, &task);
+          print_error("[receiver] Pushed task to queue\n");
           break;
         }
     }
@@ -114,6 +125,7 @@ run_async_client (task_t *task, config_t *config)
   queue_init (&ctx.task_queue, sizeof (task_t));
   queue_init (&ctx.result_queue, sizeof (result_t));
   pthread_mutex_init (&ctx.mutex, NULL);
+  ctx.config = config;
 
   ctx.socket_fd = socket (AF_INET, SOCK_STREAM, 0);
   if (ctx.socket_fd == -1)
@@ -136,9 +148,12 @@ run_async_client (task_t *task, config_t *config)
   async_client_context_t *ctx_ptr = &ctx;
 
   thread_create (&ctx.thread_pool, task_receiver, &ctx_ptr, sizeof (ctx_ptr));
+  print_error("Created receiver thread\n");
   thread_create (&ctx.thread_pool, result_sender, &ctx_ptr, sizeof (ctx_ptr));
+  print_error("Created sender thread\n");
   create_threads (&ctx.thread_pool, config->number_of_threads - 2,
                   client_worker, &ctx_ptr, sizeof (ctx_ptr));
+  print_error("Created worker threads\n");
 
 end:
   shutdown (ctx.socket_fd, SHUT_RDWR);
