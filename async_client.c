7201740dab25f862286dd247393cce352f94450c
ArtemiyGranat <artemiigranat@gmail.com>
2024-04-28 17:35:24 +0400

    add status checks

--- async_client.c
+++ async_client.c
@@ -25,13 +25,15 @@ client_worker (void *arg)
   while (true)
     {
       task_t task;
-      queue_pop (&ctx->task_queue, &task);
+      if (queue_pop (&ctx->task_queue, &task) != QS_SUCCESS)
+        return (NULL);
       print_error ("[worker] After task_queue pop\n");
       bool is_found
           = brute (&task, ctx->config, st_password_check, &st_context);
       task.task.is_correct = is_found;
 
-      queue_push (&ctx->result_queue, &task.task);
+      if (queue_push (&ctx->result_queue, &task.task) != QS_SUCCESS)
+        return (NULL);
       print_error ("[worker] After result_queue push\n");
     }
   return (NULL);
@@ -71,9 +73,9 @@ task_receiver (void *arg)
               print_error ("Could not handle hash\n");
               goto end;
             }
-          print_error (
-              "[acl receiver] Received hash %s at %p, ctx->config->hash is %p\n",
-              hash, hash, ctx->config->hash);
+          print_error ("[acl receiver] Received hash %s at %p, "
+                       "ctx->config->hash is %p\n",
+                       hash, hash, ctx->config->hash);
           ctx->config->hash = hash;
           break;
         case CMD_EXIT:
@@ -87,18 +89,24 @@ task_receiver (void *arg)
               goto end;
             }
           print_error ("[acl receiver] Received task\n");
-          queue_push (&ctx->task_queue, &task);
+
+          if (queue_push (&ctx->task_queue, &task) != QS_SUCCESS)
+            goto end;
           print_error ("[acl receiver] Pushed task to queue\n");
+
           break;
         }
     }
 
 end:
-  print_error("[acl receiver] end mark\n");
+  print_error ("[acl receiver] end mark\n");
   ctx->done = true;
-  pthread_cond_signal (&ctx->cond_sem);
+  if (pthread_cond_signal (&ctx->cond_sem) != 0)
+    {
+      print_error ("Could not signal on a conditional semaphore\n");
+    }
 
-  print_error("[acl receiver] after signal\n");
+  print_error ("[acl receiver] after signal\n");
 
   return (NULL);
 }
@@ -111,9 +119,15 @@ result_sender (void *arg)
   result_t task;
   while (true)
     {
-      queue_pop (&ctx->result_queue, &task);
+      if (queue_pop (&ctx->result_queue, &task) != QS_SUCCESS)
+        return (NULL);
       print_error ("[acl sender] After result_queue pop\n");
-      send_wrapper (ctx->socket_fd, &task, sizeof (task), 0);
+
+      if (send_wrapper (ctx->socket_fd, &task, sizeof (task), 0) == S_FAILURE)
+        {
+          print_error ("Could not send result to server\n");
+          return (NULL);
+        }
       print_error ("[acl sender] After task send\n");
     }
 
@@ -125,11 +139,31 @@ run_async_client (config_t *config)
 {
   async_client_context_t ctx;
 
-  thread_pool_init (&ctx.thread_pool);
-  queue_init (&ctx.task_queue, sizeof (task_t));
-  queue_init (&ctx.result_queue, sizeof (result_t));
-  pthread_mutex_init (&ctx.mutex, NULL);
-  pthread_cond_init (&ctx.cond_sem, NULL);
+  if (thread_pool_init (&ctx.thread_pool) == S_FAILURE)
+    {
+      print_error ("Could not initialize thread pool\n");
+      return (false);
+    }
+  if (queue_init (&ctx.task_queue, sizeof (task_t)) != QS_SUCCESS)
+    {
+      print_error ("Could not initialize task queue\n");
+      return (false);
+    }
+  if (queue_init (&ctx.result_queue, sizeof (result_t)) != QS_SUCCESS)
+    {
+      print_error ("Could not initialize result queue\n");
+      return (false);
+    }
+  if (pthread_mutex_init (&ctx.mutex, NULL) != 0)
+    {
+      print_error ("Could not initialize mutex\n");
+      return (false);
+    }
+  if (pthread_cond_init (&ctx.cond_sem, NULL) != 0)
+    {
+      print_error ("Could not initialize conditional semaphore\n");
+      return (false);
+    }
   ctx.config = config;
   ctx.done = false;
 
@@ -177,13 +211,19 @@ run_async_client (config_t *config)
 
   pthread_cleanup_pop (!0);
 
-  print_error("[async client] After wait\n");
+  print_error ("[async client] After wait\n");
 
-  thread_pool_cancel (&ctx.thread_pool);
+  if (thread_pool_cancel (&ctx.thread_pool) == S_FAILURE)
+    {
+      print_error ("Could not cancel thread pool\n");
+      return (false);
+    }
 
   print_error ("[async client] After thread pool cancel\n");
 
   shutdown (ctx.socket_fd, SHUT_RDWR);
-  close (ctx.socket_fd);
+  if (close (ctx.socket_fd) != 0)
+    print_error ("Could not close socket\n");
+
   return (false);
 }
