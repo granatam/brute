973512207547f4ec90058d31a482af8733cca511
ArtemiyGranat <artemiigranat@gmail.com>
2024-06-02 07:12:34 +0400

    add mutex lock error log

--- queue.c
+++ queue.c
@@ -191,6 +191,7 @@ queue_push_back (queue_t *queue, void *payload)
   bool list_used = true;
   if (pthread_mutex_lock (&queue->head_mutex) != 0)
     {
+      error ("Could not lock queue head mutex");
       return (QS_FAILURE);
     }
   pthread_cleanup_push (cleanup_mutex_unlock, &queue->head_mutex);
