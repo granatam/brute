f41179cf073937cb7d8852a344200287ab5c9446
Artemiy Granat <artemiigranat@gmail.com>
2024-06-05 15:44:35 +0000

    comment out another wait

--- thread_pool.c
+++ thread_pool.c
@@ -248,21 +248,21 @@ thread_pool_collect (thread_pool_t *thread_pool, bool cancel)
       pthread_cleanup_pop (!0);
     }
 
-  // if (pthread_mutex_lock (&thread_pool->mutex) != 0)
-  //   {
-  //     error ("Could not lock a mutex");
-  //     return (S_FAILURE);
-  //   }
-  // pthread_cleanup_push (cleanup_mutex_unlock, &thread_pool->mutex);
+  if (pthread_mutex_lock (&thread_pool->mutex) != 0)
+    {
+      error ("Could not lock a mutex");
+      return (S_FAILURE);
+    }
+  pthread_cleanup_push (cleanup_mutex_unlock, &thread_pool->mutex);
 
-  // while (thread_pool->count != 0)
-  //   if (pthread_cond_wait (&thread_pool->cond, &thread_pool->mutex) != 0)
-  //     {
-  //       error ("Could not wait for a conditional semaphore");
-  //       return (S_FAILURE);
-  //     }
+  while (thread_pool->count != 0)
+    if (pthread_cond_wait (&thread_pool->cond, &thread_pool->mutex) != 0)
+      {
+        error ("Could not wait for a conditional semaphore");
+        return (S_FAILURE);
+      }
 
-  // pthread_cleanup_pop (!0);
+  pthread_cleanup_pop (!0);
 
   /* Valgrind tests are returning false positive result, since it doesn't
    * wait until all memory allocated for threads is cleared, so we need to
