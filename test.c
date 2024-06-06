3968a20908ee20b6ceebb9b705f4d72c7e0287ca
Artemiy Granat <artemiigranat@gmail.com>
2024-06-06 02:39:11 +0000

    test pthreads on freebsd

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int main() {
+    pthread_mutex_t mutex;
+    if (pthread_mutex_init (&mutex, NULL) != 0) {
+        fprintf (stderr, "Could not initialize a mutex\n");
+    }
+
+    if (pthread_mutex_lock (&mutex) != 0) {
+        fprintf (stderr, "Could not lock a mutex\n");
+    }
+
+    printf ("FreeBSD test\n");
+
+    if (pthread_mutex_unlock (&mutex) != 0) {
+        fprintf (stderr, "Could not unlock a mutex\n");
+    }
+}
