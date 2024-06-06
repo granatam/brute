c4ebf03c4cf11562bf3d4bff94c5d40d5863209f
ArtemiyGranat <artemiigranat@gmail.com>
2024-06-07 02:49:13 +0400

    add log level to log message

--- log.c
+++ log.c
@@ -7,20 +7,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-__attribute__ ((format (printf, 4, 5))) status_t
-message_impl (const char *file_name, const char *func_name, int line,
-              const char *msg, ...)
+__attribute__ ((format (printf, 5, 6))) status_t
+message_impl (const char *log_level, const char *file_name,
+              const char *func_name, int line, const char *msg, ...)
 {
-  char log[1 << 7];
-  if (sprintf (log, "(%s %s %d)", file_name, func_name, line) < 0)
-    if (fprintf (stderr, "(%s %s %d)", file_name, func_name, line) < 0)
+  char log_info[1 << 7];
+  if (sprintf (log_info, "[%s] (%s %s %d)", log_level, file_name, func_name,
+               line)
+      < 0)
+    if (fprintf (stderr, "[%s] (%s %s %d)", log_level, file_name, func_name,
+                 line)
+        < 0)
       return (S_FAILURE);
 
   va_list args;
   va_start (args, msg);
 
   char message[1 << 7];
-  if (vsprintf (message, msg, args) < 0)
+  if (vsnprintf (message, sizeof (message), msg, args) < 0)
     if (vfprintf (stderr, msg, args) < 0)
       {
         return (S_FAILURE);
@@ -28,7 +32,7 @@ message_impl (const char *file_name, const char *func_name, int line,
 
   va_end (args);
 
-  fprintf (stderr, "%s %s\n", log, message);
+  fprintf (stderr, "%s %s\n", log_info, message);
 
   fflush (stderr);
 
--- log.h
+++ log.h
@@ -3,11 +3,18 @@
 
 #include "common.h"
 
-#define LOG_MESSAGE(...)                                                      \
-  message_impl (__FILE__, __func__, __LINE__, __VA_ARGS__)
+#define LOG_MESSAGE(log_level, ...)                                           \
+  message_impl (log_level, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #define IGNORE_MESSAGE(...)
 
-#define TRACE_TRACE LOG_MESSAGE
+#define LOG_TRACE(...) LOG_MESSAGE ("TRACE", __VA_ARGS__)
+#define LOG_DEBUG(...) LOG_MESSAGE ("DEBUG", __VA_ARGS__)
+#define LOG_INFO(...) LOG_MESSAGE ("INFO", __VA_ARGS__)
+#define LOG_WARN(...) LOG_MESSAGE ("WARNING", __VA_ARGS__)
+#define LOG_ERROR(...) LOG_MESSAGE ("ERROR", __VA_ARGS__)
+#define LOG_FATAL(...) LOG_MESSAGE ("FATAL", __VA_ARGS__)
+
+#define TRACE_TRACE LOG_TRACE
 #define TRACE_DEBUG IGNORE_MESSAGE
 #define TRACE_INFO IGNORE_MESSAGE
 #define TRACE_WARN IGNORE_MESSAGE
@@ -15,44 +22,44 @@
 #define TRACE_FATAL IGNORE_MESSAGE
 #define TRACE_OFF IGNORE_MESSAGE
 
-#define DEBUG_TRACE LOG_MESSAGE
-#define DEBUG_DEBUG LOG_MESSAGE
+#define DEBUG_TRACE LOG_DEBUG
+#define DEBUG_DEBUG LOG_DEBUG
 #define DEBUG_INFO IGNORE_MESSAGE
 #define DEBUG_WARN IGNORE_MESSAGE
 #define DEBUG_ERROR IGNORE_MESSAGE
 #define DEBUG_FATAL IGNORE_MESSAGE
 #define DEBUG_OFF IGNORE_MESSAGE
 
-#define INFO_TRACE LOG_MESSAGE
-#define INFO_DEBUG LOG_MESSAGE
-#define INFO_INFO LOG_MESSAGE
+#define INFO_TRACE LOG_INFO
+#define INFO_DEBUG LOG_INFO
+#define INFO_INFO LOG_INFO
 #define INFO_WARN IGNORE_MESSAGE
 #define INFO_ERROR IGNORE_MESSAGE
 #define INFO_FATAL IGNORE_MESSAGE
 #define INFO_OFF IGNORE_MESSAGE
 
-#define WARN_TRACE LOG_MESSAGE
-#define WARN_DEBUG LOG_MESSAGE
-#define WARN_INFO LOG_MESSAGE
-#define WARN_WARN LOG_MESSAGE
+#define WARN_TRACE LOG_WARN
+#define WARN_DEBUG LOG_WARN
+#define WARN_INFO LOG_WARN
+#define WARN_WARN LOG_WARN
 #define WARN_ERROR IGNORE_MESSAGE
 #define WARN_FATAL IGNORE_MESSAGE
 #define WARN_OFF IGNORE_MESSAGE
 
-#define ERROR_TRACE LOG_MESSAGE
-#define ERROR_DEBUG LOG_MESSAGE
-#define ERROR_INFO LOG_MESSAGE
-#define ERROR_WARN LOG_MESSAGE
-#define ERROR_ERROR LOG_MESSAGE
+#define ERROR_TRACE LOG_ERROR
+#define ERROR_DEBUG LOG_ERROR
+#define ERROR_INFO LOG_ERROR
+#define ERROR_WARN LOG_ERROR
+#define ERROR_ERROR LOG_ERROR
 #define ERROR_FATAL IGNORE_MESSAGE
 #define ERROR_OFF IGNORE_MESSAGE
 
-#define FATAL_TRACE LOG_MESSAGE
-#define FATAL_DEBUG LOG_MESSAGE
-#define FATAL_INFO LOG_MESSAGE
-#define FATAL_WARN LOG_MESSAGE
-#define FATAL_ERROR LOG_MESSAGE
-#define FATAL_FATAL LOG_MESSAGE
+#define FATAL_TRACE LOG_FATAL
+#define FATAL_DEBUG LOG_FATAL
+#define FATAL_INFO LOG_FATAL
+#define FATAL_WARN LOG_FATAL
+#define FATAL_ERROR LOG_FATAL
+#define FATAL_FATAL LOG_FATAL
 #define FATAL_OFF IGNORE_MESSAGE
 
 #define PASTE2(...) PASTE2_ (__VA_ARGS__)
@@ -69,7 +76,7 @@
 #define error(...) PASTE2 (ERROR, LOG_LEVEL) (__VA_ARGS__)
 #define fatal(...) PASTE2 (FATAL, LOG_LEVEL) (__VA_ARGS__)
 
-status_t message_impl (const char *file_name, const char *func_name, int line,
-                       const char *msg, ...);
+status_t message_impl (const char *log_level, const char *file_name,
+                       const char *func_name, int line, const char *msg, ...);
 
 #endif /* LOG_H */
