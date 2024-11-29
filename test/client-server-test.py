b6b0c9135121caf93d245e9d0ee326ec83fa52f6
Artemiy Granat <artemiigranat@gmail.com>
2024-11-29 14:32:06 +0300

    Add reactor server tests

--- test/client-server-test.py
+++ test/client-server-test.py
@@ -133,4 +133,68 @@ def test_two_async_clients_sync_server(data):
     ).run()
 
 
+# Reactor server, one synchronous client
+@given(data=data())
+@settings(deadline=timedelta(seconds=5), phases=phases)
+def test_sync_client_reactor_server(data):
+    _TestRunner(
+        data,
+        Config(
+            (2, 3),
+            (2, 3),
+            run_mode=RunMode.REACTOR_SERVER,
+            client_run_modes=[RunMode.SYNC_CLIENT],
+            port=9015,
+        ),
+    ).run()
+
+
+# Reactor server, two synchronous clients
+@given(data=data())
+@settings(deadline=timedelta(seconds=5), phases=phases)
+def test_two_sync_clients_reactor_server(data):
+    _TestRunner(
+        data,
+        Config(
+            (4, 5),
+            (4, 5),
+            run_mode=RunMode.REACTOR_SERVER,
+            client_run_modes=[RunMode.SYNC_CLIENT, RunMode.SYNC_CLIENT],
+            port=9016,
+        ),
+    ).run()
+
+
+# Reactor server, one asynchronous client
+@given(data=data())
+@settings(deadline=timedelta(seconds=5), phases=phases)
+def test_async_client_reactor_server(data):
+    _TestRunner(
+        data,
+        Config(
+            (2, 3),
+            (2, 3),
+            run_mode=RunMode.REACTOR_SERVER,
+            client_run_modes=[RunMode.SYNC_CLIENT],
+            port=9017,
+        ),
+    ).run()
+
+
+# Reactor server, two asynchronous clients
+@given(data=data())
+@settings(deadline=timedelta(seconds=5), phases=phases)
+def test_two_async_clients_reactor_server(data):
+    _TestRunner(
+        data,
+        Config(
+            (4, 5),
+            (4, 5),
+            run_mode=RunMode.REACTOR_SERVER,
+            client_run_modes=[RunMode.ASYNC_CLIENT, RunMode.ASYNC_CLIENT],
+            port=9018,
+        ),
+    ).run()
+
+
 # TODO: Ultimate test - 4 clients, 1 server and long password?
