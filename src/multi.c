f08137533fa6cdfbe19387d7b7e25b9005544497
Artemiy Granat <artemiigranat@gmail.com>
2026-04-14 23:16:29 +0300

    Remove thread_pool_cancel and fix hang in reactor server

--- src/multi.c
+++ src/multi.c
@@ -39,7 +39,6 @@ mt_context_init (mt_context_t *context, config_t *config)
   context->config = config;
   context->passwords_remaining = 0;
   context->password[0] = 0;
-  context->cancel_tp = true;
 
   return (S_SUCCESS);
 }
@@ -48,20 +47,12 @@ status_t
 mt_context_destroy (mt_context_t *context)
 {
   trace ("Destroying multithreading context");
-  if (context->cancel_tp)
-    {
-      if (thread_pool_cancel (&context->thread_pool) == S_FAILURE)
-        {
-          error ("Could not cancel a thread pool");
-          return (S_FAILURE);
-        }
-    }
-  else if (thread_pool_join (&context->thread_pool) == S_FAILURE)
+  if (thread_pool_join (&context->thread_pool) == S_FAILURE)
     {
       error ("Could not cancel a thread pool");
       return (S_FAILURE);
     }
-  trace ("Cancelled thread pool, destroying global queue");
+  trace ("Waited for all threads to end, destroying global queue");
 
   if (queue_destroy (&context->queue) == QS_FAILURE)
     {
